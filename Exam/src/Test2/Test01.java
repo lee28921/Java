27212530b4764f96cc24773a5b07c1d2587272ca
dlwlals6790 <dlwlals6790@gmail.com>
2023-06-15 14:02:32 +0900

    Test2 문제 추가

---
+++ Exam/src/Test2/Test01.java
@@ -0,0 +1,25 @@
+package Test2;
+
+/**
+ * 
+ * 날짜: 2023/06/15
+ * 이름: 이지민
+ * 내용: 자바 배열 문자 출력
+ *
+ */
+public class Test01 {
+	public static void main(String[] args) {
+		char[] str = {'I',' ','L','O','V','E',' ','Y','O','U'};
+		int row, col;
+		
+		for (row =1;row <10;row++) {
+			
+			for(col =0;col<=row;col++) {
+				System.out.print(str[col]);
+			}
+			System.out.print("\n");
+		}
+		
+		
+	}
+}
