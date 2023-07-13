c0a85b75b7c097fd7664f1e606b310632e9acc17
dlwlals6790 <dlwlals6790@gmail.com>
2023-07-13 17:45:55 +0900

    JDBC Shop 예제

---
+++ Shop/src/com/shop/dao/ProductDAO.java
@@ -0,0 +1,33 @@
+package com.shop.dao;
+
+import java.util.List;
+
+import com.shop.db.DBHelper;
+import com.shop.vo.ProductVO;
+
+public class ProductDAO extends DBHelper {
+	// 싱글톤
+	private static ProductDAO instance = new ProductDAO();
+	public static ProductDAO getinstance() {
+		return instance;
+	}
+	private ProductDAO() {}
+	
+	
+	//기본 CRUD 메서드
+	public int insertProduct(ProductVO vo) {
+		return 0;
+	}
+	public ProductVO selectProduct(String prodNo) {
+		return null;
+	}
+	public List<ProductVO> selectProducts() {
+		return null;
+	}
+	public int updateProduct(ProductVO vo) {
+		return 0;
+	}
+	public int deleteProduct(String prodNo) {
+		return 0;
+	}
+}
